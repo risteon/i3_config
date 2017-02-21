5f9f270a657de427ba031ef7bd34ab0df2265c1c
Christoph Rist <rist.christoph@gmail.com>
2017-02-21 21:15:19 +0100

    pystatus: add bitcoin module

--- .i3/pystatus_conf.py
+++ .i3/pystatus_conf.py
@@ -34,15 +34,6 @@ status.register("temp",
 # goes below 5 percent while discharging. The block will also color RED.
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
-status.register("battery",
-    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
 
 # Displays whether a DHCP client is running
 status.register("runwatch",
@@ -57,13 +48,8 @@ status.register("runwatch",
 #
 # Note: the network module requires PyPI package netifaces
 status.register("network",
-    interface="enp0s25",
+    interface="enp4s0",
     format_up="{v4cidr}",)
-
-# Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-    interface="wlp3s0",
-    format_up="{essid} {quality:03.0f}%",)
 
 # Shows disk usage of /
 # Format:
@@ -78,9 +64,9 @@ status.register("disk",
 status.register("pulseaudio",
     format="♪{volume}",)
 
-status.register("backlight",
-    format="☼{percentage}%",
-    backlight="intel_backlight",)
+# Bitcoin
+status.register("bitcoin", format="EUR/BTC {last_price}{status}", currency='EUR')
+
 
 status.run()
 
