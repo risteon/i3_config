33238d156161e9dcef005a32333fe97579490739
Christoph Rist <c.rist@posteo.de>
2021-10-20 21:56:27 +0200

    i3pystatus: update ifaces, add battery and backlight

--- .i3/pystatus_conf.py
+++ .i3/pystatus_conf.py
@@ -34,6 +34,15 @@ status.register("mem", format="MEM {percent_used_mem}%",)
 # goes below 5 percent while discharging. The block will also color RED.
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
+status.register("battery",
+    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },)
 
 # Displays whether a DHCP client is running
 status.register("runwatch",
@@ -48,8 +57,13 @@ status.register("runwatch",
 #
 # Note: the network module requires PyPI package netifaces
 status.register("network",
-    interface="enp4s0",
+    interface="enp0s25",
     format_up="{v4cidr}",)
+
+# Note: requires both netifaces and basiciw (for essid and quality)
+status.register("network",
+    interface="wlp3s0",
+    format_up="{essid} {quality:03.0f}%",)
 
 # Shows disk usage of /
 # Format:
@@ -64,6 +78,9 @@ status.register("disk",
 status.register("pulseaudio",
     format="♪{volume}",)
 
+status.register("backlight",
+    format="☼{percentage}%",
+    backlight="intel_backlight",)
 
 status.run()
 
